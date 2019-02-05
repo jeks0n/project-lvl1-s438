b97bcc8e697efc919cb9925490925e64e3fa7236
Evgenii Popov <jekson87@me.com>
2019-02-05 22:31:29 +0300

    2nd point corrected according to the mentor's comments

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
+import greetingsChat from '..';
 
 console.log('Welcome to the Brain Games!');
 console.log('');
 
-import readlineSync from '..';
-
-readlineSync();
+greetingsChat();
